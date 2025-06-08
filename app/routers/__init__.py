740ac14ac720a4a6cedd2aa4a7a15b39565ada40
jaehyeon1002 <100886827+jaehyeon1002@users.noreply.github.com>
2025-06-08 13:32:06 +0900

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,9 @@
 # 이 파일은 app.routers 디렉토리를 파이썬 패키지로 인식하게 합니다. 
+
+# 모든 라우터 모듈을 명시적으로 가져옵니다
+from .auth import router as auth
+from .upload import router as upload
+from .convert import router as convert
+from .convert_svc import router as convert_svc
+from .split import router as split
+from .train import router as train 
