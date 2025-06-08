ed61cf24f46e5957b2f759f84af3ba33fcb9384b
jaehyeon1002 <100886827+jaehyeon1002@users.noreply.github.com>
2025-06-08 13:19:48 +0900

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,8 @@
-# 이 파일은 app.utils 디렉토리를 파이썬 패키지로 인식하게 합니다. 
+# 이 파일은 app.utils 디렉토리를 파이썬 패키지로 인식하게 합니다.
+import os
+
+# YouTube API 키 설정
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")  # 환경 변수에서 가져오기
+
+# utils.py에서 모든 함수와 변수를 가져옵니다
+from ..utils import * 
